f6ccd1ef2988e6874f53927c138ad20fa860ecb8
System Administrator <root@Dannyyusis-MacBook-Pro.local>
2022-10-23 19:20:02 +0200

    Route protection + migration

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+//public routes
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::resource('/tasks', TasksController::class);
+//private routes
+
+Route::group(['middleware' => ['auth:sanctum']], function (){
+    Route::resource('/tasks', TasksController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
