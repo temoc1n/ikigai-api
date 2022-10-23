e8f6eef10613bf4d410daad5c28cbc7299390519
System Administrator <root@Dannyyusis-MacBook-Pro.local>
2022-10-23 19:05:55 +0200

    Login controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,17 +3,30 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\LoginUserRequest;
 use App\Traits\HttpResponses;
+use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login()
+    public function login(LoginUserRequest $request)
     {
-        return 'Login Methods here :P';
+        $request->validated($request->all());
+
+        if(!Auth::attempt($request->only(['email','password'])))
+        {
+            return $this->error('','Credentials do not match',401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+        return $this->succes([
+            'user' => $user,
+            'token' => $user->createToken($user->name)->plainTextToken
+        ]);
     }
 
     public function register(StoreUserRequest $request)
